a4595eb1386d77a8836cebb3b39470f6ab980eec
Patryk Syc <patryk.syc@smtsoftware.com>
2015-08-06 07:59:55 +0200

    System helper

---
+++ src/Edefine/Framework/Tests/Utils/SystemTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Edefine\Framework\Tests\Routing;
+
+use Edefine\Framework\Utils\Git;
+use Edefine\Framework\Utils\System;
+
+class SystemTest extends \PHPUnit_Framework_TestCase
+{
+    public function testChangingDirectories()
+    {
+        $system = new System();
+
+        $rootDir = getcwd();
+        $testsDir = sprintf('%s/src/Edefine/Framework/Tests', $rootDir);
+
+        $this->assertEquals($rootDir, $system->pwd());
+
+        $system->cd('src/Edefine');
+        $system->cd('Framework');
+        $system->cd('Tests');
+
+        $this->assertEquals($testsDir, $system->pwd());
+    }
+
+    public function testExecute()
+    {
+        $system = new System();
+
+        $returnCode = $system->execute('echo "Hello, World!"');
+
+        $this->assertEquals(0, $returnCode);
+        $this->assertEquals(['Hello, World!'], $system->getOutput());
+    }
+}
---
+++ src/Edefine/Framework/Utils/System.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Edefine\Framework\Utils;
+
+/**
+ * Class System
+ * @package Edefine\Framework\Utils
+ */
+class System
+{
+    private $output;
+
+    /**
+     * @param $path
+     */
+    public function cd($path)
+    {
+        chdir($path);
+    }
+
+    /**
+     * @return string
+     */
+    public function pwd()
+    {
+        return getcwd();
+    }
+
+    /**
+     * @param $command
+     * @return int
+     */
+    public function execute($command)
+    {
+        $this->output = null;
+
+        exec($command, $this->output, $returnCode);
+
+        return $returnCode;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
+}
